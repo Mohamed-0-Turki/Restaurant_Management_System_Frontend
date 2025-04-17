09a015e57f50d2a3c957a4c7826d35c137053f10
Mohamed-0-Turki <mohamedtarek234234@gmail.com>
2025-04-17 15:55:18 +0200

    chore: setup TanStack Query and wrap app with QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <QueryClientProvider client={queryClient}>
+        <Example />
+      </QueryClientProvider>
     </>
   )
 }
