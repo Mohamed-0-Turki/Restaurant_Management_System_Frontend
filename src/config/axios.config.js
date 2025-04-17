590ca58fdd0cac9d821d96fa82daf41a8d7caa99
Mohamed-0-Turki <mohamedtarek234234@gmail.com>
2025-04-17 16:38:15 +0200

    chore: add .env file and update axios config to use environment variables

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,1 +1,20 @@
-console.log("axios config");
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 403) {
+      window.location.href = "/forbidden"; // redirect to your 403 page
+    }
+    if (error.response?.status >= 500) {
+      window.location.href = "/server-error"; // redirect to your 403 page
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
