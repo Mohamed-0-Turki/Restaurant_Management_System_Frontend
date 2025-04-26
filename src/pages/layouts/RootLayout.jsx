97169695151ecd54897844ca5b71fab37e6fb799
Mohamed-0-Turki <mohamedtarek234234@gmail.com>
2025-04-26 05:20:47 +0300

    refactor: update RootLayout to support session handling

--- src/pages/layouts/RootLayout.jsx
+++ src/pages/layouts/RootLayout.jsx
@@ -2,10 +2,17 @@ import { ToastContainer } from 'react-toastify';
 import { Outlet, ScrollRestoration } from 'react-router';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useSelector } from 'react-redux';
+import AdminSidebar from './AdminSidebar';
 
 const RootLayout = () => {
+  const { role } = useSelector((state) => state.auth);  // Now inside a component
+
+
   return (
     <div className="flex min-h-screen">
+
+      {role === "admin" && <AdminSidebar /> }
 
       {/* Main Content Wrapper */}
       <div className="flex flex-col w-full">
